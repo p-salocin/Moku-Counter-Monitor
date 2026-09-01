4f019c4595475a2499dad748fe6d29b82c8858e0
Nicolas Pessoa <nicolas.pessoa00@gmail.com>
2026-09-01 15:20:40 +0200

    Refractor: Uptaded buttons fucntions and defined connection to Moku

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import customtkinter as ctk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkinter import messagebox
+from moku.instruments import TimeFrequencyAnalyzer
 
 # Set the apperance mode of the application
 ctk.set_appearance_mode("dark")
@@ -17,6 +19,13 @@ class live_counter_gui(ctk.CTk):
         # Define the layout of the window. 
         self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(1, weight=1)
+
+        # Global variables to hold the Moku connection and data
+        self.moku_ip = None
+        self.record_time = None
+        self.input_channel = None
+        self.continuous_time_trend = None
+        self.tfa = None
 
         # Create a frame for the configuration options on the left side of the window
         self.config_frame = ctk.CTkFrame(self, width=180)
@@ -37,17 +46,19 @@ class live_counter_gui(ctk.CTk):
 
     def create_config_widgets(self):
 
-        config_label_moku = ctk.CTkLabel(self.config_frame, text="MOKU IP", font=ctk.CTkFont(size=16, weight="bold"))
+        config_label_moku = ctk.CTkLabel(self.config_frame, text="MOKU IPv6", font=ctk.CTkFont(size=16, weight="bold"))
         config_label_moku.pack(pady=(20, 10), padx=20)
 
         self.ip_entry = ctk.CTkEntry(self.config_frame, textvariable=ctk.StringVar(), placeholder_text="Enter Moku IP")
         self.ip_entry.pack(pady=(0, 20), padx=20)
+        self.ip_entry.set("fe80::7269:79ff:feb0:dce") # Default IP for the Moku Pro in the lab
 
         record_label = ctk.CTkLabel(self.config_frame, text="Record Time (s)", font=ctk.CTkFont(size=16, weight="bold"))
         record_label.pack(pady=(0, 10), padx=20)
 
         self.record_entry = ctk.CTkEntry(self.config_frame, textvariable=ctk.StringVar(), placeholder_text="Enter Record Time")
         self.record_entry.pack(pady=(0, 20), padx=20)
+        self.record_entry.set("60") # Default record time
 
         input_label = ctk.CTkLabel(self.config_frame, text="Input Channel", font=ctk.CTkFont(size=16, weight="bold"))
         input_label.pack(pady=(0, 10), padx=20)
@@ -69,23 +80,36 @@ class live_counter_gui(ctk.CTk):
         self.input3_radio.grid(row=1, column=0, pady=5)
         self.input4_radio.grid(row=1, column=1, padx=(1,0), pady=5)
 
+        self.input1_radio.set(True)
+
         self.checkbox_time_trend = ctk.CTkCheckBox(self.config_frame,
                                                     text=f"Continuous \n Time Trend",
                                                     font=ctk.CTkFont(size=12, weight="bold"))
         self.checkbox_time_trend.pack(pady=(0, 20), padx=20)
 
-        self.button_save = ctk.CTkButton(self.config_frame, text="Save", font=ctk.CTkFont(size=12, weight="bold"))
+        self.button_save = ctk.CTkButton(self.config_frame, text="Connect", font=ctk.CTkFont(size=12, weight="bold"), command=self.get_config_info)
         self.button_save.pack(pady=(0, 20), padx=20, side="bottom")
 
     def create_button_widgets(self):
-        buttons_symbols = ["▶", "⏸", "↶", "↓"]
-        button_width = 30
-        button_height = 30
 
-        for i, symbol in enumerate(buttons_symbols):
-            button = ctk.CTkButton(self.button_frame, text=symbol, width=button_width, height=button_height,
-                                   font=ctk.CTkFont(size=20, weight="bold"))
-            button.grid(row=0, column=i, padx=(10 if i == 0 else 5, 10), pady=5)
+        button_size = 30
+        button_radius = 30
+
+        # Create the buttons 
+        self.play_button = ctk.CTkButton(self.button_frame, text="▶", height=button_size, width=button_size, font=ctk.CTkFont(size=20, weight="bold"), corner_radius=button_radius, command=self.play)
+
+        self.pause_button = ctk.CTkButton(self.button_frame, text="⏸", height=button_size, width=button_size, font=ctk.CTkFont(size=20, weight="bold"), corner_radius=button_radius)
+
+        self.rewind_button = ctk.CTkButton(self.button_frame, text="↶", height=button_size, width=button_size, font=ctk.CTkFont(size=20, weight="bold"), corner_radius=button_radius)
+
+        self.download_button = ctk.CTkButton(self.button_frame, text="↓", height=button_size, width=button_size, font=ctk.CTkFont(size=20, weight="bold"), corner_radius=button_radius)
+
+        # Place the buttons on the grid
+
+        self.play_button.grid(row=0, column=0, padx=(10, 10), pady=(5, 5))
+        self.pause_button.grid(row=0, column=1, padx=(5, 10), pady=(5, 5))
+        self.rewind_button.grid(row=0, column=2, padx=(5, 10), pady=(5, 5))
+        self.download_button.grid(row=0, column=3, padx=(5, 10), pady=(5, 5))
 
     def create_graph(self):
         # Create a matplotlib figure and axis
@@ -100,6 +124,24 @@ class live_counter_gui(ctk.CTk):
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(fill="both", expand=True)
 
+    def get_config_info(self):
+        # Store the configuration values
+        self.moku_ip = self.ip_entry.get()
+        self.record_time = self.record_entry.get()
+        self.input_channel = self.input_select.get()
+        self.continuous_time_trend = self.checkbox_time_trend.get()
+
+        self.connect_to_moku()
+
+    def connect_to_moku(self):
+        if self.moku_ip:
+            try:
+                self.tfa = TimeFrequencyAnalyzer(f"{self.moku_ip}", force_connect=True)
+                messagebox.showinfo("Connection Successful", f"Successfully connected to Moku at {self.moku_ip}")
+            except Exception as e:
+                messagebox.showerror("Connection Failed", f"Failed to connect to Moku at {self.moku_ip}. {e}. Please verify your connection.")
+        else:
+            messagebox.showwarning("Connection Warning", "Moku IP is not set. Please enter a valid the IPv6 address.")
 
 
 root = live_counter_gui()
